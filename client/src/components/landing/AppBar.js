3559751991b06aafcaf63f1f128ecbcbe522c1fb
Yunah Kim <yunah.kim@stonybrook.edu>
2024-02-04 07:40:16 -0500

    edit app name

--- client/src/components/landing/AppBar.js
+++ client/src/components/landing/AppBar.js
@@ -33,7 +33,7 @@ export default function NavBar() {
           className="main_logo"
           alt="logo"
         />
-        Mentallyyy
+        Mindbloom.ai
       </div>
 
       {valueFromLocalStorage === null ? (
